c624d03da8773433cf8e5c22e7885900c99f3c05
Eduar Apaza Yáñez <eduarapazayanez@Eduars-Mac-Studio.local>
2023-04-14 21:24:46 -0500

    added tests

--- src/components/Product/__tests__/Product.test.jsx
+++ src/components/Product/__tests__/Product.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import Product from '../index';
 
 beforeEach(() => {
@@ -27,5 +27,27 @@ describe('<Product />', () => {
 
   test('always shows its price', () => {
     expect(screen.getByText('S/ 769.90')).toBeDefined();
+  });
+
+  test('the default value of favorite icon is outlined heart icon', () => {
+    expect(screen.getByTestId('FavoriteBorderOutlinedIcon')).toBeDefined();
+  });
+
+  test('when the user clicks on favorite button, the icon changes to filled heart', () => {
+    const button = screen.getByTestId('favorite-button');
+
+    fireEvent.click(button);
+
+    expect(screen.getByTestId('FavoriteIcon')).toBeDefined();
+  });
+
+  test('when the user clicks on favorite button it returns the outlined icon if the icon is filled', () => {
+    const button = screen.getByTestId('favorite-button');
+
+    fireEvent.click(button);
+
+    fireEvent.click(button);
+
+    expect(screen.getByTestId('FavoriteBorderOutlinedIcon')).toBeDefined();
   });
 });
